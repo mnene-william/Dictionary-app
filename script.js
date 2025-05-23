1968665eb252bea8101a79785929bcd6c14943c9
mnene-william <mnenewilliam26@gmail.com>
2025-05-23 15:23:44 +0300

    add a copy to clipboard funtion

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const quoteText = document.querySelector("#quote");
 const authorName = document.querySelector(".author")
 const newQuoteBtn = document.querySelector("#newQuote");
+const soundBtn = document.querySelector(".sound");
+const copyBtn = document.querySelector(".copy");
 
 
  function getNewQuote() {
     newQuoteBtn.innerHTML = "Loading..."
+    newQuoteBtn.classList.add("loading");
 
     fetch("https://api.api-ninjas.com/v1/quotes", {
         headers: {
@@ -16,8 +19,19 @@ const newQuoteBtn = document.querySelector("#newQuote");
          quoteText.innerHTML = result[0].quote;
          authorName.innerHTML = result[0].author;
          newQuoteBtn.innerHTML = "New Quote"
+         newQuoteBtn.classList.remove("loading");
     })
 
  }
 
 newQuoteBtn.addEventListener("click", getNewQuote);
+
+soundBtn.addEventListener("click", ()=> {
+    let words = new SpeechSynthesisUtterance(`${quoteText.innerHTML} by ${authorName.innerHTML}`);
+    speechSynthesis.speak(words);
+})
+
+copyBtn.addEventListener("click", () =>{
+    navigator.clipboard.writeText(quoteText.innerHTML);
+}
+)
